640b2b568eeef137e872af0d103fe719f8bd95a5
ChandraRizki <crizkipradana@gmail.com>
2019-06-16 18:37:19 +0700

    membuat layout kategori emas
    terdapat tiga layout yang saya tambahkan.

---
+++ android/EGOLD/app/src/main/java/com/example/e_gold/cincin.java
@@ -0,0 +1,14 @@
+package com.example.e_gold;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class cincin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cincin);
+    }
+
+}
