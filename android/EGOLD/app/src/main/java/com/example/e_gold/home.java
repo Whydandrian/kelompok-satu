8d0f6076aa065617e0e5dc3e988051433975201e
ChandraRizki <crizkipradana@gmail.com>
2019-06-18 19:07:46 +0700

    add icon dan update file activity

--- android/EGOLD/app/src/main/java/com/example/e_gold/home.java
+++ android/EGOLD/app/src/main/java/com/example/e_gold/home.java
@@ -1,7 +1,10 @@
 package com.example.e_gold;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView kategori = (ImageView) findViewById(R.id.kategori);
+
+        kategori.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(home.this, menukategori.class);
+                startActivity(intent);
+            }
+        });
     }
 }
