8c5d84110961d9e52bf1295570ba979c02e037be
ekasafitri <ekasafitri4299@gmail.com>
2019-06-18 09:55:05 +0700

    Update halaman login
    menambahkan toast login gagal

--- android/EGOLD/app/src/main/java/com/example/e_gold/MainActivity.java
+++ android/EGOLD/app/src/main/java/com/example/e_gold/MainActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // deklarasi tombol
     private Button btn_login, btn_register;
+    private EditText email, password;
 
 
     @Override
@@ -20,12 +23,24 @@ public class MainActivity extends AppCompatActivity {
         btn_login = (Button) findViewById(R.id.btnLogin);
         btn_register = (Button) findViewById(R.id.btnRegister);
 
+        email = (EditText) findViewById(R.id.txtemail);
+        password = (EditText) findViewById(R.id.etPassword);
+
         // function tombol
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent iLogin = new Intent(getApplicationContext(), menukategori.class);
-                startActivity(iLogin);
+
+                if(email.getText().toString().equals("safitrieka656@gmail.com")&& password.getText().toString().equals("12345")){
+                    Intent email = new Intent(MainActivity.this, menukategori.class);
+                    startActivity(email);
+
+                    Toast.makeText(getApplicationContext(),"Login Berhasil!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Login Gagal", Toast.LENGTH_SHORT).show();
+                    email.setText("");
+                    password.setText("");
+                }
             }
         });
 
