fb4406097b17c759198524a29e2bd7802fac7746
Whydandrian <whydandrian@gmail.com>
2019-06-20 12:04:03 +0700

    update project

---
+++ android/EGOLD/app/src/main/java/com/tugasbesar/kelompoksatu/egold/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.tugasbesar.kelompoksatu.egold;
+
+public class DashboardActivity extends BaseActivity {
+
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_dashboard;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.navigation_dashboard;
+    }
+
+}
