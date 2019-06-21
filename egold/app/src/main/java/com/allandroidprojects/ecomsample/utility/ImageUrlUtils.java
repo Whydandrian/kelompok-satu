fafca2974db11456d5b68ec12f6f616c9523fdb0
Whydandrian <whydandrian@gmail.com>
2019-06-21 15:59:28 +0700

    add files

---
+++ egold/app/src/main/java/com/allandroidprojects/ecomsample/utility/ImageUrlUtils.java
@@ -0,0 +1,158 @@
+package com.allandroidprojects.ecomsample.utility;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 06peng on 2015/6/24.
+ */
+public class ImageUrlUtils {
+    static ArrayList<String> wishlistImageUri = new ArrayList<>();
+    static ArrayList<String> cartListImageUri = new ArrayList<>();
+
+    public static String[] getImageUrls() {
+        String[] urls = new String[] {
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting5.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin5.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung5.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang5.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin5.jpg",
+
+        };
+        return urls;
+    }
+
+    public static String[] getHomeApplianceUrls() {
+        String[] urls = new String[]{
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung5.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin5.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getLifeStyleUrls() {
+        String[] urls = new String[]{
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang5.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getAntingUrls() {
+        String[] urls = new String[]{
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/anting5.jpg",
+        };
+        return urls;
+    }
+
+    public static String[] getCincinUrls() {
+        String[] urls = new String[]{
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/cincin5.jpg",
+        };
+        return urls;
+    }
+
+    public static String[] getGelangUrls() {
+        String[] urls = new String[]{
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/gelang5.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getKalungUrls() {
+        String[] urls = new String[]{
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/kalung5.jpg",
+        };
+        return urls;
+    }
+
+    public static String[] getLiontinUrls() {
+        String[] urls = new String[]{
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin1.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin2.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin3.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin4.jpg",
+                "https://raw.githubusercontent.com/Whydandrian/kelompok-satu/master/android/EGOLD/app/src/main/res/drawable/liontin5.jpg",
+        };
+        return urls;
+    }
+
+    // Methods for Wishlist
+    public void addWishlistImageUri(String wishlistImageUri) {
+        this.wishlistImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeWishlistImageUri(int position) {
+        this.wishlistImageUri.remove(position);
+    }
+
+    public ArrayList<String> getWishlistImageUri(){ return this.wishlistImageUri; }
+
+    // Methods for Cart
+    public void addCartListImageUri(String wishlistImageUri) {
+        this.cartListImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeCartListImageUri(int position) {
+        this.cartListImageUri.remove(position);
+    }
+
+    public ArrayList<String> getCartListImageUri(){ return this.cartListImageUri; }
+}
